57f44900f39b6ea147dce72ce6e69bc1ab706d09
Samuel Viana <sdviana@ualg.pt>
2014-08-29 20:06:38 +0100

    Redudant logging code
    Remove error_log's , now everything goes to stdout when debug is true

--- lib/InvoiceXpressRequest.php
+++ lib/InvoiceXpressRequest.php
@@ -1,4 +1,5 @@
 <?php
+
 //include_once('XmlDomConstruct.php');
 /**
  * A simple PHP API wrapper for the InvoiceXpress API.
@@ -12,148 +13,149 @@
  * @license    MIT
  * @version    1.0
  */
-class InvoiceXpressRequestException extends Exception {}
-class InvoiceXpressRequest {
-	/*
-	 * The domain you need when making a request
-	*/
-	protected static $_domain = '';
-	
-	/*
-	 * The API token you need when making a request
-	*/
-	protected static $_token = '';
-	
-	/*
-	 * The API url we're hitting. {{ DOMAIN }} will get replaced with $domain
-	* when you set InvoiceXpressRequest::init($domain, $token)
-	*/
-	protected $_api_url = 'https://{{ DOMAIN }}.invoicexpress.net/{{ CLASS }}.xml';
-	
-	/*
-	 * Stores the current method we're using. Example:
-	* new InvoiceXpressRequest('client.create'), 'client.create' would be the method
-	*/
-	protected $_method = '';
-	
-	/*
-	 * Any arguments to pass to the request
-	*/
-	protected $_args = array();
-	
-	/*
-	 * Determines whether or not the request was successful
-	*/
-	protected $_success = false;
-	
-	/*
-	 * Holds the error returned from our request
-	*/
-	protected $_error = '';
-	
-	/*
-	 * Holds the response after our request
-	*/
-	protected $_response = array();
+class InvoiceXpressRequestException extends Exception {
+    
+}
 
+class InvoiceXpressRequest {
+    /*
+     * The domain you need when making a request
+     */
+
+    protected static $_domain = '';
+
+    /*
+     * The API token you need when making a request
+     */
+    protected static $_token = '';
+
+    /*
+     * The API url we're hitting. {{ DOMAIN }} will get replaced with $domain
+     * when you set InvoiceXpressRequest::init($domain, $token)
+     */
+    protected $_api_url = 'https://{{ DOMAIN }}.invoicexpress.net/{{ CLASS }}.xml';
+
+    /*
+     * Stores the current method we're using. Example:
+     * new InvoiceXpressRequest('client.create'), 'client.create' would be the method
+     */
+    protected $_method = '';
+
+    /*
+     * Any arguments to pass to the request
+     */
+    protected $_args = array();
+
+    /*
+     * Determines whether or not the request was successful
+     */
+    protected $_success = false;
+
+    /*
+     * Holds the error returned from our request
+     */
+    protected $_error = '';
+
+    /*
+     * Holds the response after our request
+     */
+    protected $_response = array();
     protected $_debug = FALSE;
-	
-	/*
-	 * Initialize the and store the domain/token for making requests
-	*
-	* @param string $domain The subdomain like 'yoursite'.freshbooks.com
-	* @param string $token The token found in your account settings area
-	* @return null
-	*/
-	public static function init($domain, $token)
-	{
-		self::$_domain = $domain;
-		self::$_token = $token;
-	}
-	
-	/*
-	 * Set up the request object and assign a method name
-	*
-	* @param array $method The method name from the API, like 'client.update' etc
-	* @return null
-	*/
-	public function __construct($method)
-	{
-		$this->_method = $method;
-	}
+
+    /*
+     * Initialize the and store the domain/token for making requests
+     *
+     * @param string $domain The subdomain like 'yoursite'.freshbooks.com
+     * @param string $token The token found in your account settings area
+     * @return null
+     */
+
+    public static function init($domain, $token) {
+        self::$_domain = $domain;
+        self::$_token = $token;
+    }
+
+    /*
+     * Set up the request object and assign a method name
+     *
+     * @param array $method The method name from the API, like 'client.update' etc
+     * @return null
+     */
+
+    public function __construct($method) {
+        $this->_method = $method;
+    }
 
     public function withDebug($yes) {
         $this->_debug = $yes;
     }
-	
-	/*
-	 * Set the data/arguments we're about to request with
-	*
-	* @return null
-	*/
-	public function post($data)
-	{
-		$this->_args = $data;
-	}
-	
-	/*
-	 * Determine whether or not it was successful
-	*
-	* @return bool
-	*/
-	public function success()
-	{
-		return $this->_success;
-	}
-	
-	/*
-	 * Get the error (if there was one returned from the request)
-	*
-	* @return string
-	*/
-	public function getError()
-	{
-		return $this->_error;
-	}
-	
-	/*
-	 * Get the response from the request we made
-	*
-	* @return array
-	*/
-	public function getResponse()
-	{
-		return $this->_response;
-	}
+
+    /*
+     * Set the data/arguments we're about to request with
+     *
+     * @return null
+     */
+
+    public function post($data) {
+        $this->_args = $data;
+    }
+
+    /*
+     * Determine whether or not it was successful
+     *
+     * @return bool
+     */
+
+    public function success() {
+        return $this->_success;
+    }
+
+    /*
+     * Get the error (if there was one returned from the request)
+     *
+     * @return string
+     */
+
+    public function getError() {
+        return $this->_error;
+    }
+
+    /*
+     * Get the response from the request we made
+     *
+     * @return array
+     */
+
+    public function getResponse() {
+        return $this->_response;
+    }
 
     /**
-    * Get the original XML answer from the InvoiceXpress API
-    * 
-    */
-
+     * Get the original XML answer from the InvoiceXpress API
+     * 
+     */
     public function getResponseXml() {
         return $this->_serverAnswer;
     }
-	
-	/*
-	 * Get the generated XML to view. This is useful for debugging
-	 * to see what you're actually sending over the wire. Call this
-	 * after $ie->post() but before your make your $ie->request()
-	 *
-	 * @return array
-	 */
-	public function getGeneratedXML()
-	{
-	
-		$dom = new XmlDomConstruct('1.0', 'utf-8');
-		$dom->fromMixed($this->_args);
-		$post_data = $dom->saveXML();
-	
-		return $post_data;
-	
-	}
-	
-	    /**
+
+    /*
+     * Get the generated XML to view. This is useful for debugging
+     * to see what you're actually sending over the wire. Call this
+     * after $ie->post() but before your make your $ie->request()
+     *
+     * @return array
+     */
+
+    public function getGeneratedXML() {
+
+        $dom = new XmlDomConstruct('1.0', 'utf-8');
+        $dom->fromMixed($this->_args);
+        $post_data = $dom->saveXML();
+
+        return $post_data;
+    }
+
+    /**
      * invoiceMethods
      *
      * Handle all Invoice & Simplified invoices requests
@@ -192,24 +194,23 @@ class InvoiceXpressRequest {
                 break;
         }
 
-    
+
 
         return $url;
     }
 
-
-public function itemMethods($ch, $class, $url, $id, $extra = '')  {
-    $methodName = $class[1];
-    switch ($methodName) {
+    public function itemMethods($ch, $class, $url, $id, $extra = '') {
+        $methodName = $class[1];
+        switch ($methodName) {
             case 'list':
                 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-                $url = str_replace('{{ CLASS }}',$class[0], $url);               
+                $url = str_replace('{{ CLASS }}', $class[0], $url);
                 break;
-    }
+        }
 
-    
-    return $url;
-}
+
+        return $url;
+    }
 
     /**
      * clientMethods
@@ -234,23 +235,23 @@ public function itemMethods($ch, $class, $url, $id, $extra = '')  {
                 $url = str_replace('{{ CLASS }}', $class[0], $url);
 
                 //curl_setopt($ch, CURLOPT_POST, 0);
-               
+
 
                 break;
             case 'get':
                 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
                 $url = str_replace('{{ CLASS }}', "clients/" . $id, $url);
-                
+
                 break;
             case 'update':
                 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
                 $url = str_replace('{{ CLASS }}', "clients/" . $id, $url);
-                
+
                 break;
             case 'invoices':
                 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
                 $url = str_replace('{{ CLASS }}', "clients/" . $id . "/" . $class[1], $url);
-                
+
                 break;
             case 'find-by-name':
                 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
@@ -268,15 +269,15 @@ public function itemMethods($ch, $class, $url, $id, $extra = '')  {
             case 'create-debit-note':
                 list($before, $after) = explode('-', $class[1], 2);
                 $url = str_replace('{{ CLASS }}', "clients/" . $id . "/" . $before . "/" . $after, $url);
-                
+
                 break;
         }
 
 
         return $url;
     }
-	
-	/**
+
+    /**
      * request
      *
      * Send the request over the wire
@@ -288,10 +289,8 @@ public function itemMethods($ch, $class, $url, $id, $extra = '')  {
      * 
      * @return  array
      */
-	public function request($id = '',$extra='')
-	{		
-		if(!self::$_domain || !self::$_token)
-		{
+    public function request($id = '', $extra = '') {
+        if (!self::$_domain || !self::$_token) {
             throw new InvoiceXpressRequestException('You need to call InvoiceXpressRequest::init($domain, $token) with your domain and token.');
         }
 
@@ -303,7 +302,8 @@ public function itemMethods($ch, $class, $url, $id, $extra = '')  {
 
         $class = explode(".", $this->_method);
 
-        if ($this->_debug) echo ("=> METHOD " . print_r($class,TRUE));
+        if ($this->_debug)
+            echo ("=> METHOD " . print_r($class, TRUE));
 
         $ch = curl_init();    // initialize curl handle
         //Filter correct method to run and return $url
@@ -320,19 +320,18 @@ public function itemMethods($ch, $class, $url, $id, $extra = '')  {
                 break;
             default:
                 echo ("The methods for the {$class[0]} were not implemented yet!");
-            break;
-
+                break;
         }
-           
 
 
 
-         $url .= "?api_key=" . self::$_token;
 
-        
-         if ($this->_debug) {
-           echo ("==> URL = ".$url . "\n");
-     }
+        $url .= "?api_key=" . self::$_token;
+
+
+        if ($this->_debug) {
+            echo ("==> URL = " . $url . "\n");
+        }
         curl_setopt($ch, CURLOPT_URL, $url); // set url to post to
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // return into a variable
         curl_setopt($ch, CURLOPT_TIMEOUT, 40); // times out after 40s
@@ -342,7 +341,7 @@ public function itemMethods($ch, $class, $url, $id, $extra = '')  {
         //curl_setopt($ch, CURLOPT_SSLCERT, "cacert.pem");
         //curl_setopt($ch, CURLOPT_SSLCERTTYPE, 'PEM');
 
-		if ($class[1] != "get")
+        if ($class[1] != "get")
             curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data); // add POST fields
         curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/xml; charset=utf-8"));
 
@@ -350,13 +349,10 @@ public function itemMethods($ch, $class, $url, $id, $extra = '')  {
         $this->_serverAnswer = $result;
         $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
-		if(curl_errno($ch))
-		{
+        if (curl_errno($ch)) {
             $this->_error = 'A cURL error occured: ' . curl_error($ch);
             return;
-		}
-		else
-		{
+        } else {
             curl_close($ch);
         }
 
@@ -364,20 +360,41 @@ public function itemMethods($ch, $class, $url, $id, $extra = '')  {
         // a user_meta wc_ie_client_id defined that not exists in InvoiceXpress
         if ($result && $result != " ") {
             $res = print_r($result, true);
-            error_log("result = {" . $res . "}");
+            if ($this->_debug) {
+                echo("result string = {" . $res . "}");
+            }
+
+            libxml_use_internal_errors(true);
 
             $response = json_decode(json_encode(simplexml_load_string($result)), true);
+
+            $xml_errors = libxml_get_errors();
+
+            if (count($xml_errors) > 0) {
+                $errors_string = array_map(function($error) {
+                    return $error->line . " : " .  $error->message;
+                }, $xml_errors);
+                $this->_error = implode('\n', $errors_string);
+                libxml_clear_errors();
+                throw new InvoiceXpressRequestException("Empty or invalid XML response : " . $this->_error 
+                        . "\n Server answer:' " . $result . " ' ");
+            }
             $r = print_r($response, true);
-            error_log("response = " . $r);
+            if ($this->_debug) {
+                echo("response = " . $r);
+            }
 
             $this->_response = $response;
         }
 
         $this->_success = (($http_status == '201 Created') || ($http_status == '200 OK'));
-        error_log("http status = " . $http_status);
+        if ($this->_debug) {
+            echo("http status = " . $http_status . "\n");
+        }
 
         if (isset($response['error'])) {
             $this->_error = $response['error'];
         }
     }
+
 }
